d19168eb73bc1a21b13943cc2f393c171fc539ef
somasundar sithivinayagam <sundar.soma.1997@outlook.com>
2022-01-10 06:52:40 +0530

    ep 14

--- app.ts
+++ app.ts
@@ -1,12 +1,21 @@
-function add(n1: number, n2: number) {
-    console.log(typeof(n1));
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw 'incorrect input'
+function add(n1: number, n2: number, showResult: boolean) {
+    console.log(typeof (n1));
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw 'incorrect input'
+    // }
+    const result = n1 + n2;
+    if (showResult) {
+        console.log('Result is: ' + result);
+        console.log("Result is: " + result);
+        console.log(`Result is: ${result}`);
     }
-    return n1 + n2;
+    else {
+        return n1 + n2;
+    }
 }
 
-const number1 = '5';
+const number1 = 5; // 5.0
 const number2 = 2.5;
+const printResult = true;
 
-alert(add(number1,number2));
+alert(add(number1, number2, printResult));
