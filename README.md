d7ccd121b1ac68bfeb92ef95bc49bb64102c3775
somasundar sithivinayagam <sundar.soma.1997@outlook.com>
2022-01-10 06:17:43 +0530

    Episode 11

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # learning-ts
+
+# Types
+
+# 1. number 
+    5, 7.6, -10
+
+# 2. string
+    'Hi', "Hi", `Hi`
+
+# 3. boolean
+    true or false
--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-alert("hello World!...");
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.5;
+
+alert(add(number1,number2));
