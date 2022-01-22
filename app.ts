e54d6c455326d184c2bee9af302ad244a70ed56e
somasundar sithivinayagam <sundar.soma.1997@outlook.com>
2022-01-22 17:48:54 +0530

    object types and nested object types

--- app.ts
+++ app.ts
@@ -1,25 +1,24 @@
-function add(n1: number, n2: number, showResult: boolean) {
-    console.log(typeof (n1));
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //     throw 'incorrect input'
-    // }
-    const result = n1 + n2;
-    if (showResult) {
-        console.log('Result is: ' + result);
-        console.log("Result is: " + result);
-        console.log(`Result is: ${result}`);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-// type inference auto detects types 
-// let number1;
-// number1 = '5';
-let number1: number;
-number1 = 5;
-// number1 = '5';
-const number2 = 2.5;
-const printResult = true;
+const Person: object = {
+    Name: 'Somasundar',
+    Age: 24
+};
 
-alert(add(number1, number2, printResult));
+const Person1: {
+    Name: string;
+    Age: number;
+} = {
+    Name: 'Somasundar',
+    Age: 24
+};
+
+const Person2: {
+    Name: string;
+    Age: number;
+} = {
+    Name: 'Somasundar',
+    Age: 24
+};
+
+console.log(Person.Name);
+console.log(Person1.Name);
+console.log(Person2.Name);
